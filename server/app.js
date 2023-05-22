266f5b272e6b5eb048f0ca0aa88cb975ca7fdf5b
Martin Schröter <martin.schroeter@diva-e.com>
2023-05-22 22:17:33 +0200

    minor typo fixes

--- server/app.js
+++ server/app.js
@@ -10,7 +10,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const port = 3000
 
 const app = express()
-
 const render = custom_renderer({
   adobe_launch: process.env["ADOBE_LAUNCH_PROPERTY"],
   usercentrics_settings_id: process.env["USERCENTRICS_SETTINGS_ID"],
